1df28be5f61f25b8bf79025b008d8523db85d26b
Filippo <pippo.faccini@gmail.com>
2023-11-25 17:14:33 +0100

    fix: inverter speed conversion

--- src/can_messages.c
+++ src/can_messages.c
@@ -42,11 +42,11 @@ void can_messages_parse(can_message_t *message, can_data_t *can_data) {
 	}
 #else
 	if (message->socket == CAN_SOCKET_PRIMARY) {
-		if (primary_id_is_message(message->frame.can_id)) {
-			can_messages_parse_primary(message, can_data);
-		} else if (inverters_id_is_message(message->frame.can_id)) {
+		if (inverters_id_is_message(message->frame.can_id)) {
 			can_messages_parse_inverters(message, can_data);
-		}
+		} else if (primary_id_is_message(message->frame.can_id)) {
+			can_messages_parse_primary(message, can_data);
+		} 
 	} else if (message->socket == CAN_SOCKET_SECONDARY) {
 		if (secondary_id_is_message(message->frame.can_id)) {
 			can_messages_parse_secondary(message, can_data);
@@ -190,7 +190,7 @@ static inline void can_messages_parse_inverters(can_message_t *message, can_data
 	}
 }
 
-static inline double inverter_convert_speed(double val) { return (val * 10.0f * 4.5 * (M_PI / 60.0) * (INV_MAX_SPEED / 32767.f)); }
+static inline double inverter_convert_speed(double val) { return (val * 4.5 * (M_PI / 60.0) * (INV_MAX_SPEED / 32767.f)); }
 static inline double convert_gyro(double val) { return - val * M_PI / 180.0; };
 static inline double convert_accel(double val) { return - val * 9.81; };
 static inline double convert_brake(double val) { return val / 100.0; };
